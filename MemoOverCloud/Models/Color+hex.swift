869cab193b14f8870664c18b5812dd614a149f11
0xF0D0 <goodbumsu@gmail.com>
2018-02-23 10:53:04 +0900

    Code from Dori
    Signed-off-by: 0xF0D0 <goodbumsu@gmail.com>

---
+++ MemoOverCloud/Models/Color+hex.swift
@@ -0,0 +1,24 @@
+//
+// Created by 김범수 on 2018. 2. 23..
+// Copyright (c) 2018 piano. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    /// Init UIColor with hex string
+    convenience init(hex: String) {
+        let scan = Scanner(string: hex.trimmingCharacters(in: .newlines))
+        var color: UInt32 = 0
+        scan.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue = CGFloat(b) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
