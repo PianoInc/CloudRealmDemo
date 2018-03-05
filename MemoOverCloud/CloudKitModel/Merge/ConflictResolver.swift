46e4ae33744ce3abf8460da343d396fbed063e5f
0xF0D0 <goodbumsu@gmail.com>
2018-03-05 09:44:14 +0900

    resolve attribute conflict with range info

--- MemoOverCloud/CloudKitModel/Merge/ConflictResolver.swift
+++ MemoOverCloud/CloudKitModel/Merge/ConflictResolver.swift
@@ -13,33 +13,107 @@ class ConflictResolver {
         guard let myModified = myRecord.modificationDate,
               let serverModified = serverRecord.modificationDate else {return false}
 
-
-        if myModified.compare(serverModified) == .orderedDescending {
-            //my Win!
-            switch ancestor.recordType {
-                case RealmCategoryModel.recordTypeString:
-                    serverRecord[Schema.Category.name] = myRecord[Schema.Category.name]
-
-                case RealmNoteModel.recordTypeString:
-                    serverRecord[Schema.Note.title] = myRecord[Schema.Note.title]
-
-                    //TODO: add attribute to merge
-                    let ancestorContent = ancestor[Schema.Note.content] as! String
-                    let myContent = myRecord[Schema.Note.content] as! String
-                    let serverContent = serverRecord[Schema.Note.content] as! String
-                    serverRecord[Schema.Note.content] = Diff3.merge(ancestor: ancestorContent, a: myContent, b: serverContent) as CKRecordValue
-
-                    serverRecord[Schema.Note.categoryRecordName] = myRecord[Schema.Note.categoryRecordName]
-
-                case RealmCategoryForSharedModel.recordTypeString:
-                    serverRecord[Schema.categoryForSharedNote.CategoryRecordName] = myRecord[Schema.categoryForSharedNote.CategoryRecordName]
-
-                default: break
+        switch ancestor.recordType {
+        case RealmNoteModel.recordTypeString:
+            return mergeNote(ancestor: ancestor, myRecord: myRecord, serverRecord: serverRecord, myModified: myModified, serverModified: serverModified)
+            
+        case RealmCategoryModel.recordTypeString:
+            
+            if myModified.compare(serverModified) == .orderedDescending {
+                serverRecord[Schema.Category.name] = myRecord[Schema.Category.name]
             }
-            return true
-        } else {
-            //server Win!
-            return false
+            
+        case RealmCategoryForSharedModel.recordTypeString:
+            
+            if myModified.compare(serverModified) == .orderedDescending {
+                serverRecord[Schema.categoryForSharedNote.CategoryRecordName] = myRecord[Schema.categoryForSharedNote.CategoryRecordName]
+            }
+        
+        default: break
         }
+
+        return myModified.compare(serverModified) == .orderedDescending
     }
+    
+    private static func mergeNote(ancestor: CKRecord, myRecord: CKRecord, serverRecord: CKRecord, myModified: Date, serverModified: Date) -> Bool {
+        var flag = false
+        if serverRecord.changedKeys().contains(Schema.Note.content) ||
+            serverRecord.changedKeys().contains(Schema.Note.attributes) {
+            
+            flag = true
+            
+            let ancestorContent = ancestor[Schema.Note.content] as! String
+            
+            let myContent = myRecord[Schema.Note.content] as! String
+            let serverContent = serverRecord[Schema.Note.content] as! String
+            
+            let myAttributesData = myRecord[Schema.Note.attributes] as! Data
+            let myAttributes = try! JSONDecoder().decode([PianoAttribute].self, from: myAttributesData)
+            
+            let serverAttributesData = serverRecord[Schema.Note.attributes] as! Data
+            let serverAttributes = try! JSONDecoder().decode([PianoAttribute].self, from: serverAttributesData)
+            
+            let myAttributedString = NSMutableAttributedString(string: myContent)
+            myAttributes.forEach {myAttributedString.add(attribute: $0)}
+            
+            let serverAttributedString = NSMutableAttributedString(string: serverContent)
+            serverAttributes.forEach {serverAttributedString.add(attribute: $0)}
+            
+            let chunks = Diff3.merge(ancestor: ancestorContent, a: myContent, b: serverContent)
+            chunks.forEach {
+                switch $0 {
+                //TODO: make Notification here
+                case .conflict(let original, let my, let server, let myRange, let serverRange):
+                    //resolve conflict if possible
+                    
+                    if my == server { //false conflict
+                        return
+                    } else if my == original { //server add
+                        let replacementString = serverAttributedString.attributedSubstring(from: serverRange)
+                        myAttributedString.replaceCharacters(in: myRange, with: replacementString)
+                    } else if server == original{ //my add
+                        return
+                    } else {
+                        //true conflict!!
+                        let myReplacementString = myAttributedString.attributedSubstring(from: myRange)
+                        let serverReplacementString = serverAttributedString.attributedSubstring(from: serverRange)
+                        
+                        let conflictString = NSMutableAttributedString(string: "!@#$ Conflict!!\nMy\n========================\n")
+                        
+                        conflictString.append(myReplacementString)
+                        conflictString.append(NSAttributedString(string: "\nServer\n========================\n"))
+                        conflictString.append(serverReplacementString)
+                        conflictString.append(NSAttributedString(string: "\n========================\n"))
+                        
+                        myAttributedString.replaceCharacters(in: myRange, with: conflictString)
+                    }
+                    
+                default: break
+                }
+            }
+            
+            var attributes:[PianoAttribute] = []
+            
+            myAttributedString.enumerateAttributes(in: NSMakeRange(0, myAttributedString.length), options: .reverse) { (dic, range, _) in
+                for (key, value) in dic {
+                    if let pianoAttribute = PianoAttribute(range: range, attribute: (key, value)) {
+                        attributes.append(pianoAttribute)
+                    }
+                }
+            }
+            
+            serverRecord[Schema.Note.content] = myAttributedString.string as CKRecordValue
+            serverRecord[Schema.Note.attributes] = ((try? JSONEncoder().encode(attributes)) ?? Data()) as CKRecordValue
+        }
+        
+        
+        if myModified.compare(serverModified) == .orderedDescending {
+            flag = true
+            serverRecord[Schema.Note.title] = myRecord[Schema.Note.title]
+            serverRecord[Schema.Note.categoryRecordName] = myRecord[Schema.Note.categoryRecordName]
+        }
+        
+        return flag
+    }
+    
 }
